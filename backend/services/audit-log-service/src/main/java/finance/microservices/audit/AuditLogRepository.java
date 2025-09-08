322df4b1008c22b93b8af113c9215474707e90e9
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-09-08 07:16:32 +0200

    remove audit log micro service

--- backend/services/audit-log-service/src/main/java/finance/microservices/audit/AuditLogRepository.java
+++
@@ -1,9 +0,0 @@
-
-package finance.microservices.audit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
