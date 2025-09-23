4034d42e56980a1befcc8beb86c2e8adc377addc
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-09-23 08:08:22 +0200

    add docker compose code for transaction service

---
+++ services/transaction-service/src/main/java/com/finance/transaction_service/model/Transaction.java
@@ -0,0 +1,32 @@
+package com.finance.transaction_service.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "transaction")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    private Long user_id;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
