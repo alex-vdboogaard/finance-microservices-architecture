b0e4cc2b60e7e09592e6ac6fe94928a00022bed8
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-10-13 23:37:40 +0200

    fix UUID type

--- services/audit-log-service/src/main/java/com/finance/audit_log_service/repository/AuditLogRepository.java
+++ services/audit-log-service/src/main/java/com/finance/audit_log_service/repository/AuditLogRepository.java
@@ -1,6 +1,7 @@
 package com.finance.audit_log_service.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,6 @@ import com.finance.audit_log_service.model.AuditLog;
 
 
 @Repository
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
     List<AuditLog> findByAction(String action);
 }
