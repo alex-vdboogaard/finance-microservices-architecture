479ffb09c8abb21df83edf3e3ae575eeb9a247a0
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-10-14 19:49:32 +0200

    add to do and remove empty space

--- services/audit-log-service/src/main/java/com/finance/audit_log_service/controller/AuditLogController.java
+++ services/audit-log-service/src/main/java/com/finance/audit_log_service/controller/AuditLogController.java
@@ -1,21 +1,12 @@
 package com.finance.audit_log_service.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.finance.audit_log_service.model.AuditLog;
 import com.finance.audit_log_service.service.AuditLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/logs")
@@ -32,11 +23,11 @@ public class AuditLogController {
     public ResponseEntity<List<AuditLog>> getAuditLogsByAction(@RequestParam String action) {
         return ResponseEntity.ok(auditLogService.findByAction(action));
     }
-    
+
 
     @PostMapping
     public ResponseEntity<AuditLog> createAuditLog(@RequestBody AuditLog auditLog) {
-       AuditLog created = auditLogService.createAuditLog(auditLog);
-       return ResponseEntity.ok(created);
+        AuditLog created = auditLogService.createAuditLog(auditLog);
+        return ResponseEntity.ok(created);
     }
 }
--- services/audit-log-service/src/main/java/com/finance/audit_log_service/repository/AuditLogRepository.java
+++ services/audit-log-service/src/main/java/com/finance/audit_log_service/repository/AuditLogRepository.java
@@ -1,15 +1,15 @@
 package com.finance.audit_log_service.repository;
 
-import java.util.List;
-import java.util.UUID;
-
+import com.finance.audit_log_service.model.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.finance.audit_log_service.model.AuditLog;
+import java.util.List;
+import java.util.UUID;
 
 
 @Repository
 public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
     List<AuditLog> findByAction(String action);
+    //TODO: add filter by search query
 }
