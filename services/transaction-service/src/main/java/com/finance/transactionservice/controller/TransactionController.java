1e765174c88832f782945cf85b9a70cc9ea0b7ad
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-10-19 22:06:18 +0200

    add comments to pom files to describe dependencies and add logging to transaction post route

--- services/transaction-service/src/main/java/com/finance/transactionservice/controller/TransactionController.java
+++ services/transaction-service/src/main/java/com/finance/transactionservice/controller/TransactionController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.finance.dto.ApiResponse;
 import com.finance.transactionservice.dto.TransactionResponse;
 import com.finance.transactionservice.service.TransactionService;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.finance.logging.LoggingConfig;
+import com.finance.transactionservice.model.Transaction;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
@@ -23,5 +34,24 @@ public class TransactionController {
     public ResponseEntity<List<TransactionResponse>> getAllTransactions() {
         return ResponseEntity.ok(transactionService.findAll());
     }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Transaction>> createTransaction(@RequestBody Transaction transaction, HttpServletRequest request) {
+        LoggingConfig.startRequest(request.getRequestURI(), "transaction-service");
+        log.info("Received new transaction request: {}", transaction.toString());
+        Transaction createdTransaction = transactionService.create(transaction);
+
+        ApiResponse<Transaction> response = ApiResponse.<Transaction>builder()
+                .success(true)
+                .message("Transaction created successfully")
+                .data(createdTransaction)
+                .build();
+
+        log.info("Completed transaction request: {}", createdTransaction.toString());
+        
+        LoggingConfig.endRequest();
+        return ResponseEntity.ok(response);
+    }
+    
     
 }
