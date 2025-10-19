b4a79017b6e7b549e7530ddd9caaa2574ff340dd
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-10-19 20:10:53 +0200

    add timestamp to ApiReponse model

--- common/src/main/java/com/finance/dto/ApiResponse.java
+++ common/src/main/java/com/finance/dto/ApiResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -13,4 +14,7 @@ public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
 }
