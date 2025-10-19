87028bbd4f74a62df3504f9795423d7d3daee690
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-10-19 19:11:05 +0200

    add basic shared library

--- services/audit-log-service/src/main/java/com/finance/audit_log_service/controller/AuditLogController.java
+++ services/audit-log-service/src/main/java/com/finance/audit_log_service/controller/AuditLogController.java
@@ -13,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.finance.audit_log_service.model.AuditLog;
 import com.finance.audit_log_service.service.AuditLogService;
+import com.finance.dto.ApiResponse;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tag;;
 
 @RestController
 @RequestMapping("/logs")
@@ -24,19 +25,39 @@ public class AuditLogController {
     private AuditLogService auditLogService;
 
     @GetMapping
-    public ResponseEntity<List<AuditLog>> getAuditLogs() {
-        return ResponseEntity.ok(auditLogService.getAll());
+    public ResponseEntity<ApiResponse<List<AuditLog>>> getAuditLogs() {
+        ApiResponse<List<AuditLog>> response =  ApiResponse.<List<AuditLog>>builder()
+        .success(true)
+        .message("Fetched audit logs successfully")
+        .data(auditLogService.getAll())
+        .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/action")
-    public ResponseEntity<List<AuditLog>> getAuditLogsByAction(@RequestParam String action) {
-        return ResponseEntity.ok(auditLogService.findByAction(action));
+    public ResponseEntity<ApiResponse<List<AuditLog>>> getAuditLogsByAction(@RequestParam String action) {
+        ApiResponse<List<AuditLog>> response = ApiResponse.<List<AuditLog>>builder()
+        .success(true)
+        .message("Fetched audit logs successfully")
+        .data(auditLogService.findByAction(action))
+        .build();
+
+        return ResponseEntity.ok(response);
     }
 
 
     @PostMapping
-    public ResponseEntity<AuditLog> createAuditLog(@RequestBody AuditLog auditLog) {
+    public ResponseEntity<ApiResponse<AuditLog>> createAuditLog(@RequestBody AuditLog auditLog) {
         AuditLog created = auditLogService.createAuditLog(auditLog);
-        return ResponseEntity.ok(created);
+    
+        ApiResponse<AuditLog> response = ApiResponse.<AuditLog>builder()
+                .success(true)
+                .message("Created audit log successfully")
+                .data(created)
+                .build();
+    
+        return ResponseEntity.ok(response);
     }
+    
 }
