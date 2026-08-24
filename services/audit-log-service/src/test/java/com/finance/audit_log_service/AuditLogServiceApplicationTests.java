cff5f7b38db38c8c6cfd5c99ecf02b5ecbb2132d
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2026-08-24 17:55:39 +0200

    Disable automatic DDL updates in production configuration

--- services/audit-log-service/src/test/java/com/finance/audit_log_service/AuditLogServiceApplicationTests.java
+++ services/audit-log-service/src/test/java/com/finance/audit_log_service/AuditLogServiceApplicationTests.java
@@ -3,11 +3,13 @@ package com.finance.audit_log_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.flyway.enabled=false",
+    "spring.jpa.hibernate.ddl-auto=none"
+})
 class AuditLogServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
